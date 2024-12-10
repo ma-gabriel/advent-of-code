53c1e6de0a541784efb09008afdb88d58af141f5
ma-gabriel <gabi.eymat@gmail.com>
2024-12-10 17:21:21 +0100

    hello

--- 2024/8/manda.py
+++ 2024/8/manda.py
@@ -1,21 +1,34 @@
-
+def is_valid(doc, i, j):
+    if i < 0 or j < 0:
+        return 0
+    if i >= len(doc):
+        return 0
+    if j >= len(doc[j]):
+        return 0
+    return 1
 
 total = 0
 with open("entry.txt") as my_file:
+    doc = list()
     for line in my_file:
-        res = int(line.split(':')[0])
-        facts = [int(elem) for elem in line.split(':')[1].split()]
-        l = len(facts) - 1;
-        for i in range(2 ** l):
-            chars = f"{bin(i)[2:] :0>{l}}"
-            r = facts[0]
-            for j, nb in enumerate(facts[1:]):
-                if chars[j] == '0': r += nb
-                else : r *= nb
-                if r > res: break 
-            if r == res:
-                total += res
-                break
+        doc.append(list(line)[:-1])
 
+    antennas = dict()
+    for i in range(len(coo)):
+        for j in range(len(coo[i])):
+            elem = coo[i][j]
+            if elem == '.':
+                continue
+            if elem in antennas:
+                antennas[elem].append((i, j))
+            else:
+                antennas[elem] = [(i, j)]
+    for elem, lst in antennas:
+        for antenna1 in lst:
+            for antenna2 in lst:
+                if antenna1 == antenna2:
+                    continue
+                if is_valid(doc, antenna2[0]
+    
 
 print(total)
