d7228d2699f4bfbe8e2ccac22012029c340d4e53
ma-gabriel <gabi.eymat@gmail.com>
2024-12-14 20:14:11 +0100

    OK

--- 2024/14/bonus.py
+++ 2024/14/bonus.py
@@ -1,5 +1,19 @@
-from time import sleep
-from manda import get_robot
+
+def get_robot(line):
+    space = line.find(' ')
+    robot = {"i":[int(num) for num in line[line.index("=") + 1:space].split(',')]}
+    line = line[space:]
+    robot["d"] = [int(num) for num in line[line.index("=") + 1:].split(',')]
+    return (robot)
+
+def  ChristmasTree(floor):
+    for i in range(len(floor)):
+        if "".join(floor[i]).find("🤖" * 29) != -1:
+            return True
+    for i in range(len(floor)):
+        for j in range(len(floor[i])):
+            floor[i][j] = '  '
+    return False
 
 if __name__ == "__main__":
     robots = list()
@@ -9,17 +23,15 @@ if __name__ == "__main__":
     height = 103
     seconds = 0
 
-    print("It take less than 2 minutes, it is slow because you have to CTRL+C yourself")
-    sleep(3)
-
-    while seconds < 10000:
+    floor = [[' '] * width for i in range(height)]
+    while not ChristmasTree(floor):
         seconds += 1
-        floor = [[' '] * width for i in range(height)]
         for robot in robots:
-            floor[(robot['i'][1] + robot['d'][1] * seconds) % height][(robot['i'][0] + robot['d'][0] * seconds) % width] = 'x'
-        tmp = list()
-        for line in floor:
-            tmp.append(''.join(line))
-        print(*tmp, sep='\n')
-        print(seconds, '\n') 
-        sleep(0.01)
+            floor[(robot['i'][1] + robot['d'][1] * seconds) % height][(robot['i'][0] + robot['d'][0] * seconds) % width] = '🤖'
+        if seconds == 10000:
+            print("If the entry is does not work, you might have an infinite loop. My robots already formed the easter egg")
+
+    for line in floor:
+        print(''.join(line))
+    print("Merry Christmas")
+    print(seconds)
