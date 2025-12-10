0aeba18aa32fa257a0bfa9e9e9433bcae9eca98a
ma-gabriel <gabi.eymat@gmail.com>
2025-12-10 07:24:35 +0100

    improved 2025 day 10 manda because i don't know how to do the bonus

--- 2025/10/manda.py
+++ 2025/10/manda.py
@@ -1,3 +1,9 @@
+def countSetBits(n):
+    count = 0
+    while n:
+        count += n & 1
+        n >>= 1
+    return count
 
 res = 0
 with open("entry.txt") as my_file:
@@ -8,19 +14,20 @@ with open("entry.txt") as my_file:
             continue
         lights, *buttons, joltage = line.split()
         lights = [False if light == '.' else True for light in lights[1:-1]]
-        buttons = tuple(tuple(int(elem) for elem in button[1:-1:2]) for button in buttons)
+        buttons = tuple(tuple(int(elem) for elem in button[1:-1].split(',')) for button in buttons)
 
         init_lights = [light for light in lights]
         lowest = 100000
-        for n in range(2 ** len(buttons)):
-            temp = 0
+        for n in range(1 << len(buttons)):
+            temp = countSetBits(n)
+            if temp >= lowest:
+                continue
             for i in range(len(buttons)):
-                if n & (2 ** i):
+                if n & (1 << i):
                     for toggle in buttons[i]:
                         lights[toggle] = not lights[toggle]
-                    temp += 1
             if not any(lights):
-                lowest = min(lowest, temp)
+                lowest = temp
             lights = [light for light in init_lights]
         res += lowest
 print(res)
